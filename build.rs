77839efdb45a329c0d6c4e52237fed8ec82d8be3
aeon <jerry73204@gmail.com>
2023-09-09 23:30:17 +0800

    Use new torch-build API

--- build.rs
+++ build.rs
@@ -1,33 +1,36 @@
 use anyhow::Result;
+use torch_build::{CppExtension, CudaExtension};
 
 static CPP_SOURCE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/csrc/nms_cpu.cpp");
 static CUDA_SOURCE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/csrc/nms_cuda.cu");
 
 fn main() -> Result<()> {
-    let mut cargo_commands = vec![];
-
-    // compile nms_cpu.cpp
-    let mut build = cc::Build::new();
-    torch_build::build_cpp(
-        &mut build,
-        true,
-        false,
-        Some(&mut cargo_commands),
-        [CPP_SOURCE],
-    )?;
-    build.try_compile("nms_cpu")?;
-
-    // compile nms_cuda.cu
-    let mut build = cc::Build::new();
-    torch_build::build_cuda(&mut build, false, Some(&mut cargo_commands), [CUDA_SOURCE])?;
-    build.try_compile("nms_cuda")?;
-
     // re-compile if C++/CUDA source changed
     println!("cargo:rerun-if-changed={}", CPP_SOURCE);
     println!("cargo:rerun-if-changed={}", CUDA_SOURCE);
-    cargo_commands.iter().for_each(|command| {
-        println!("{}", command);
-    });
+
+    // compile nms_cpu.cpp
+    {
+        let mut build = cc::Build::new();
+        let mut cpp_ext = CppExtension::new();
+        cpp_ext
+            .use_cuda_api(true)
+            .link_python(true)
+            .source(CPP_SOURCE);
+        cpp_ext.configure(&mut build)?;
+        build.try_compile("nms_cpu")?;
+        cpp_ext.link()?;
+    }
+
+    // compile nms_cuda.cu
+    {
+        let mut build = cc::Build::new();
+        let mut cuda_ext = CudaExtension::new();
+        cuda_ext.link_python(false).source(CUDA_SOURCE);
+        cuda_ext.configure(&mut build)?;
+        build.try_compile("nms_cuda")?;
+        cuda_ext.link()?;
+    }
 
     Ok(())
 }
